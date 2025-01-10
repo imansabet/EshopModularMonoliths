9f05db8ca8b6080a61ccdccc1d39c72368c7c29e
iman <dev.imansabet@gmail.com>
2025-01-10 15:17:40 +0330

    Develop AddCatalogModule extention method in CatalogModule.cs - Registering Catalog Dependencies into Program.cs

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -1,6 +1,14 @@
+﻿using Catalog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddCatalogModule(builder.Configuration);
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+ 
