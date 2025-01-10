04cde43f1875402d782e7ca08417cfbc928b1fb8
iman <dev.imansabet@gmail.com>
2025-01-10 15:31:29 +0330

    Develop AddBasketModule, AddOrderingModule extention methods - Registering Basket and Ordering Dependencies into Program.cs

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -1,9 +1,13 @@
-﻿using Catalog;
+﻿using Basket;
+using Catalog;
+using Ordering;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
-    .AddCatalogModule(builder.Configuration);
+    .AddCatalogModule(builder.Configuration)
+    .AddOrderingModule(builder.Configuration)
+    .AddBasketModule(builder.Configuration);
 
 
 var app = builder.Build();
