aa6a019a1bd159ac66a52d6a5d63eb449eabcd85
iman <dev.imansabet@gmail.com>
2025-01-12 21:48:11 +0330

    Develop DDD Rich-domain model Entity for Product Entity

--- src/Modules/Catalog/Catalog/Products/Models/Product.cs
+++ src/Modules/Catalog/Catalog/Products/Models/Product.cs
@@ -1,12 +1,39 @@
-﻿using Shared.DDD;
-
+﻿
 namespace Catalog.Products.Models;
 
 public class Product : Entity<Guid>
 {
-    public string Name { get; set; } = default!;
-    public List<string> Category { get; set; } = new();
-    public string Description { get; set; } = default!;
-    public string ImageFile { get; set; } = default!;
-    public decimal Price { get; set; }
+   
+    public string Name { get; private set; } = default!;
+    public List<string> Category { get; private set; } = new();
+    public string Description { get; private set; } = default!;
+    public string ImageFile { get; private set; } = default!;
+    public decimal Price { get; private set; }
+
+    public static Product Create(Guid id, string name, List<string> category, string description, string imageFile, decimal price) 
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+        var product = new Product
+        {
+            Id = id,
+            Name = name,
+            Category = category,
+            Description = description,
+            ImageFile = imageFile,
+            Price = price
+        };
+        return product;
+    }
+    public void Update(string name, List<string> category, string description, string imageFile, decimal price) 
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+        Name = name;
+        Category = category;
+        Description = description;
+        ImageFile = imageFile;
+        Price = price;
+    }
+
 }
