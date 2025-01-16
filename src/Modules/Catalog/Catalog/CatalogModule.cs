c00173fcbf427f613828d71d39070be8745f31e3
iman <dev.imansabet@gmail.com>
2025-01-16 15:55:04 +0330

    Register EF Core DbContext in Asp.Net DependencyInjection Tool for PostgreSQL

--- src/Modules/Catalog/Catalog/CatalogModule.cs
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -8,6 +8,17 @@ public static class CatalogModule
 {
     public static IServiceCollection AddCatalogModule(this IServiceCollection services,IConfiguration configuration) 
     {
+        // api endpoint services
+
+        // application use case services
+
+        // data-infrastructure services
+        var connectionString = configuration.GetConnectionString("Database");
+        
+        services.AddDbContext<CatalogDbContext>(options =>
+            options.UseNpgsql(connectionString));
+
+
         return services;
     }
     public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app) 
