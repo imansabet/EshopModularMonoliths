c68c099a2321260d849c7c9afbf269b1dd2ff9a5
iman <dev.imansabet@gmail.com>
2025-01-18 19:38:45 +0330

    Develop Products CreateProductCommandHandler using CatalogDbContext in Catalog

--- src/Modules/Catalog/Catalog/Features/CreateProduct/CreateProductEndpoint.cs
+++ src/Modules/Catalog/Catalog/Features/CreateProduct/CreateProductEndpoint.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Catalog.Features.CreateProduct
+﻿namespace Catalog.Features.CreateProduct
 {
     internal class CreateProductEndpoint
     {
--- src/Modules/Catalog/Catalog/Features/CreateProduct/CreateProductHandler.cs
+++ src/Modules/Catalog/Catalog/Features/CreateProduct/CreateProductHandler.cs
@@ -1,19 +1,34 @@
-﻿using MediatR;
-
-namespace Catalog.Features.CreateProduct;
+﻿namespace Catalog.Features.CreateProduct;
 
 public record CreateProductCommand
-    (string Name,List<string> Category,string Description,string ImageFile,decimal Price) 
-    : IRequest<CreateProductResult>;
+    (ProductDto  Product) 
+    : ICommand<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
 
 
-public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+public class CreateProductCommandHandler(CatalogDbContext dbContext) : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
+        var product = CreateNewProduct(command.Product);
 
+        dbContext.Products.Add(product);
+        await dbContext.SaveChangesAsync(cancellationToken);
+        return new CreateProductResult(product.Id); 
+    }
+
+    private Product CreateNewProduct(ProductDto productDto)
+    {
+        var product = Product.Create(
+                Guid.NewGuid(),
+                productDto.Name,
+                productDto.Category,
+                productDto.Description,
+                productDto.ImageFile,
+                productDto.Price
+            );
+        return product;
     }
 }
  
