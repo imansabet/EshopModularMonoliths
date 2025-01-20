ee8688144b7101e4d4eee153bd9a60913e011c24
iman <dev.imansabet@gmail.com>
2025-01-20 18:06:36 +0330

    Developing UpdateProduct Endpoint for REST APIs in Catalog Module

--- src/Modules/Catalog/Catalog/Features/UpdateProduct/UpdateProductEndpoint.cs
+++ src/Modules/Catalog/Catalog/Features/UpdateProduct/UpdateProductEndpoint.cs
@@ -1,5 +1,26 @@
-﻿namespace Catalog.Features.UpdateProduct;
+﻿
+using Catalog.Features.CreateProduct;
 
-public class UpdateProductEndpoint
+namespace Catalog.Features.UpdateProduct;
+
+public record UpdateProductRequest(ProductDto Product);
+public record UpdateProductResponse(bool IsSuccess);
+public class UpdateProductEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/products", async (UpdateProductRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<UpdateProductCommand>();
+            var result = await sender.Send(command);
+            var response = result.Adapt<UpdateProductResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("UpdateProduct")
+        .Produces<CreateProductResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Update Product")
+        .WithDescription("Update Product");
+    }
 }
