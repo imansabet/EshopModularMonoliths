f070cbf146d28704ccdf315185636f3f822728d5
iman <dev.imansabet@gmail.com>
2025-01-20 18:52:00 +0330

    Developing GetProducts Endpoint for REST APIs in Catalog Module

---
+++ src/Modules/Catalog/Catalog/Features/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,28 @@
+﻿
+using Catalog.Features.CreateProduct;
+
+namespace Catalog.Features.GetProducts;
+
+//public record GetProductsRequest();
+
+public record GetProductsResponse(IEnumerable<ProductDto> Products);
+
+
+public class GetProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async ( ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsQuery());
+
+            var response = result.Adapt<GetProductsResponse>();
+            return  Results.Ok(response);
+        })
+        .WithName("Get Products")
+        .Produces<CreateProductResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Products")
+        .WithDescription("Get Products");
+    }
+}
