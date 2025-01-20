fe04704b87c8c96abacbbf4d98390023828629fb
iman <dev.imansabet@gmail.com>
2025-01-20 19:02:03 +0330

    Developing GetProductById Endpoint for REST APIs in Catalog Module

--- src/Modules/Catalog/Catalog/Features/GetProductById/GetProductByIdEndpoint.cs
+++ src/Modules/Catalog/Catalog/Features/GetProductById/GetProductByIdEndpoint.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using Catalog.Features.CreateProduct;
+using Catalog.Features.GetProducts;
 
-namespace Catalog.Features.GetProductById
+namespace Catalog.Features.GetProductById;
+
+//public record GetProductByIdRequest();
+public record GetProductByIdResponse(ProductDto Product);
+
+
+public class GetProductByIdEndpoint : ICarterModule
 {
-    internal class GetProductByIdEndpoint
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapGet("/product/{id}",async (Guid id,ISender sender) => 
+        {
+            var result = await sender.Send(new GetProductByIdQuery(id));
+
+            var response = result.Adapt<GetProductByIdResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("GetProductById")
+        .Produces<CreateProductResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Product By Id")
+        .WithDescription("Get Product By Id");
     }
 }
