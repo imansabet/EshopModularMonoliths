7fcf022947256aca2882b7522eaebfe00741fdbf
iman <dev.imansabet@gmail.com>
2025-01-20 18:25:29 +0330

    Developing DeleteProduct Endpoint for REST APIs in Catalog Module

--- src/Modules/Catalog/Catalog/Features/DeleteProduct/DeleteProductEndpoint.cs
+++ src/Modules/Catalog/Catalog/Features/DeleteProduct/DeleteProductEndpoint.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using Catalog.Features.CreateProduct;
 
-namespace Catalog.Features.DeleteProduct
+namespace Catalog.Features.DeleteProduct;
+
+//public record DeleteProductRequest(Guid Id);
+//since the only required information is the Id of Product to be deleted which is obtain directly from url paramteres
+public record DeleteProductResponse(bool IsSuccess);
+
+
+
+public class DeleteProductEndpoint : ICarterModule
 {
-    internal class DeleteProductEndpoint
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapDelete("/products/{id}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteProductCommand(id));
+            var response = result.Adapt<DeleteProductResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("DeleteProduct")
+        .Produces<CreateProductResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Delete Product")
+        .WithDescription("Delete Product");
     }
 }
