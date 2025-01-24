8bb1349c714fd783c20bfaa221dc9acd9846cede
iman <dev.imansabet@gmail.com>
2025-01-24 12:32:46 +0330

    Develop ShoppingCart Domain Entity Models in Basket Domain Entities

---
+++ src/Modules/Basket/Basket/Basket/Models/ShoppingCart.cs
@@ -0,0 +1,11 @@
+﻿using Shared.DDD;
+
+namespace Basket.Basket.Models;
+
+public class ShoppingCart : Aggregate<Guid>
+{
+    public string UserName { get; private set; } = default!;
+    private readonly List<ShoppingCartItem> _items = new();
+    public IReadOnlyList<ShoppingCartItem> Items => _items.AsReadOnly();
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+}
---
+++ src/Modules/Basket/Basket/Basket/Models/ShoppingCartItem.cs
@@ -0,0 +1,25 @@
+﻿namespace Basket.Basket.Models;
+
+public class ShoppingCartItem
+{
+    public Guid ShoppingCartId { get; private set; } = default!;
+    public Guid ProductId { get; private set; } = default!;
+    public int Quantity { get; internal set; } = default!;
+    public string Color { get; private set; } = default!;
+
+    // From Catalog module
+    public decimal Price { get; private set; } = default!;
+    public string ProductName { get; private set; } = default!;
+
+    // Internal constructor
+    internal ShoppingCartItem(Guid shoppingCartId, Guid productId, int quantity, string color, decimal price, string productName)
+    {
+        ShoppingCartId = shoppingCartId;
+        ProductId = productId;
+        Quantity = quantity;
+        Color = color;
+        Price = price;
+        ProductName = productName;
+    }
+
+}
