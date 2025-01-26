14dc28b1adca7a5d2247e8117d455c8145ea0978
iman <dev.imansabet@gmail.com>
2025-01-26 18:02:04 +0330

    Develop CreateBasket POST Endpoint with Carter implements ICarterModule

--- src/Modules/Basket/Basket/GlobalUsing.cs
+++ src/Modules/Basket/Basket/GlobalUsing.cs
@@ -7,4 +7,11 @@ global using Basket.Basket.Dtos;
 global using FluentValidation;
 global using Shared.CQRS;
 global using Basket.Basket.Exceptions;
+global using Carter;
+global using Mapster;
+global using MediatR;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Routing;
+
 
