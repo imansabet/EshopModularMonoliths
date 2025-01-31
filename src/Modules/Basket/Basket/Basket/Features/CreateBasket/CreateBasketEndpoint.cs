a12d8a51a6c6789bbf5a9720aaa12ebc3074bdac
iman <dev.imansabet@gmail.com>
2025-01-31 19:05:31 +0330

    Secure and Test Basket Endpoints with Keycloak Integration

--- src/Modules/Basket/Basket/Basket/Features/CreateBasket/CreateBasketEndpoint.cs
+++ src/Modules/Basket/Basket/Basket/Features/CreateBasket/CreateBasketEndpoint.cs
@@ -18,7 +18,9 @@ public class CreateBasketEndpoint : ICarterModule
         .Produces<CreateBasketResponse>(StatusCodes.Status201Created)
         .ProducesProblem(StatusCodes.Status400BadRequest)
         .WithSummary("Create Basket")
-        .WithDescription("Create Basket");
+        .WithDescription("Create Basket")
+        .RequireAuthorization();
+        
 
     }
 }
