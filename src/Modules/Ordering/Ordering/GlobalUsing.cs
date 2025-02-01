4c08fbf43b560d8c8ada02564caf419c1e31a0fe
iman <dev.imansabet@gmail.com>
2025-02-01 20:38:49 +0330

    Develop Ordering Domain Entities with Tactical Domain-Driven Design Patterns

---
+++ src/Modules/Ordering/Ordering/GlobalUsing.cs
@@ -0,0 +1,8 @@
+﻿global using Shared.DDD;
+global using Ordering.Orders.Models;
+global using Ordering.Orders.ValueObjects;
+global using Ordering.Orders.Events;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Builder;
+global using MediatR;
+global using Microsoft.Extensions.Logging;
